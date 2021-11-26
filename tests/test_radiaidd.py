f6d0ceee1d6ade572687f463afa596062dd41129
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2021-11-26 20:47:22 +0100

    Added tests

---
+++ tests/test_radiaidd.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 26 20:45:18 2021
+
+@author: johan
+"""
+
+import RadiAIDD
+
+def test_radiaidd():
+
+    RadiAIDD.run()
+
+if __name__ == "__main__":
+    test_radiaidd()
